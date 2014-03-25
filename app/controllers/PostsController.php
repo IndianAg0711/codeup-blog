63ecb3e3ae49d9ccb3a5819e80b7041f3983dbae
IndianAg0711 <joseph.villafranca@gmail.com>
2014-03-25 14:40:17 -0500

    added a create post blade

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -19,7 +19,7 @@ class PostsController extends \BaseController {
 	 */
 	public function create()
 	{
-		return 'form';
+		return View::make('posts/create');;
 	}
 
 	/**
@@ -29,7 +29,7 @@ class PostsController extends \BaseController {
 	 */
 	public function store()
 	{
-		return 'store';
+		return Redirect::back()->withInput();
 	}
 
 	/**
---
+++ app/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid">
+<h1>Create New Post:</h1>
+<br>
+<br>
+	<form method="POST" enctype="multipart/form-data" action="{{{ action('PostsController@store') }}}">
+	 	<div class="form-group">
+	    	<label for="title">Title:</label>
+	    	<input type="input" class="form-control" id="title" name="title" value="{{{ Input::old('title') }}}">
+	 	</div>
+	 	<div class="form-group">
+	    	<label for="body">Post:</label>
+	    	<textarea rows="8" class="form-control" id="body" name="body">
+	    		{{{ Input::old('body') }}}
+	    	</textarea>
+	 	</div>
+	 	<button type="submit" class="btn btn-default">Submit</button>
+	</form>
+</div>
+
+
+@stop
